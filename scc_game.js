5c5c646887619d818a76303a6a55f646f299c11f
dmj19 <73169117+dmj19@users.noreply.github.com>
2021-06-06 12:39:46 +0100

    js.lint

--- scc_game.js
+++ scc_game.js
@@ -1,17 +1,67 @@
+// const scc_game = Object.create(null);
 
-const scc_game = Object.create(null);
+// const dice_set = {};
 
-const dice_set = {};
-
-scc_game.filled_dice_set = function(dice_set) {
-    var random_int= Math.floor(Math.random() * 6)+1;
-    for (var i=1; i<=5; i++) {
-        dice_set["random_int_" + String(i)] == random_int;
-    }; 
-}; 
+// scc_game.filled_dice_set = function(dice_set) {
+//     var random_int= Math.floor(Math.random() * 6)+1;
+//     for (var i=1; i<=5; i++) {
+//         dice_set["random_int_" + String(i)] == random_int;
+//     };
+// };
 
 
-console.log(scc_game.filled_dice_set())
+// console.log(scc_game.filled_dice_set())
+
+
+function diceroll (num){
+    const blank_set=[];
+    for (let i=1; i<num+1; i++){
+        blank_set.push(Math.floor(Math.random() * 6) + 1);
+    }
+    return blank_set
+  };
+
+const total= (array) => {
+    return array.reduce((a,b) => a+b,0);
+};
+
+function filter (dice_set_2){
+    if (dice_set_2.length==5){
+      if (dice_set_2.includes(6)){
+        dice_set_2.splice(dice_set_2.indexOf(6),1);
+      }};
+      if (dice_set_2.length==4){
+        if (dice_set_2.includes(5)){
+        dice_set_2.splice(dice_set_2.indexOf(5),1);
+      }
+      }
+      if (dice_set_2.length==3){
+        if (dice_set_2.includes(4)){
+        dice_set_2.splice(dice_set_2.indexOf(4),1);
+      }}
+      };
+var player = new Array(5);
+
+for (let i=1;i<4;i++){
+    if (player.length==2){
+      console.log("Would you like to keep your dice or reroll?");
+    // if yes then break here
+    };
+    player=diceroll(player.length);
+    console.log(player);
+    filter(player);
+    console.log(player);
+};
+
+
+if (player.length ==2) {
+    total(player)
+    console.log ("Your Score is "+ total(player))
+};
+if (player.length>2) {
+    console.log ("Bad Luck, No Score, Roll Again?")
+};
+
 
 
 
@@ -19,7 +69,7 @@ console.log(scc_game.filled_dice_set())
 // const random_int_7=Math.floor(Math.random() * 6)+1;
 // const random_int_8=Math.floor(Math.random() * 6)+1;
 // const random_int_9=Math.floor(Math.random() * 6)+1;
-// const random_int_10=Math.floor(Math.random() * 6)+1; 
+// const random_int_10=Math.floor(Math.random() * 6)+1;
 
 
 // const initial_dice_set= [
@@ -33,51 +83,51 @@ console.log(scc_game.filled_dice_set())
 //         console.log("Roll Again")
 //     };
 //     else {
-//         console.log("You've found a captain You can keep this score and roll again")
+//         console.log("You've found a captain!")
 //         second_dice_set= [6]
-//     }; 
-// }; 
+//     };
+// };
 
 // scc_game.filled_second_set = function(second_dice_set) {
 //     var random_int= Math.floor(Math.random() * 6)+1;
 //     for (var i=1; i<=(6-second_dice_set.length); i++) {
 //         append_dice_set["random_int_" + i] == random_int;
-//     }; 
-// }; 
+//     };
+// };
 
 
-// "random_int_1": 1 , 
+// "random_int_1": 1 ,
 // "random_int_2": 1 ,
-// "random_int_3": 1 , 
-// "random_int_4": 1 , 
+// "random_int_3": 1 ,
+// "random_int_4": 1 ,
 // "random_int_5": 1
 
 
 // Object.keys(dice_set).forEach(
 //     values => values== random_int
 // );
-// var dice_One_numbers= "dice_images/Dice_"+random_int_6+".png";
-// var dice_Two_numbers= "dice_images/Dice_"+random_int_7+".png";
-// var dice_Three_numbers= "dice_images/Dice_"+random_int_8+".png";
-// var dice_Four_numbers= "dice_images/Dice_"+random_int_9+".png";
-// var dice_Five_numbers= "dice_images/Dice_"+random_int_10+".png"; 
+var dice_One_numbers= "dice_images/Dice_"+random_int_6+".png";
+var dice_Two_numbers= "dice_images/Dice_"+random_int_7+".png";
+var dice_Three_numbers= "dice_images/Dice_"+random_int_8+".png";
+var dice_Four_numbers= "dice_images/Dice_"+random_int_9+".png";
+var dice_Five_numbers= "dice_images/Dice_"+random_int_10+".png";
 
 // var dice_One_numbers= "dice_images/Dice_"+random_int_6+".png";
 // var dice_Two_numbers= "dice_images/Dice_"+random_int_7+".png";
 // var dice_Three_numbers= "dice_images/Dice_"+random_int_8+".png";
 // var dice_Four_numbers= "dice_images/Dice_"+random_int_9+".png";
-// var dice_Five_numbers= "dice_images/Dice_"+random_int_10+".png"; 
+// var dice_Five_numbers= "dice_images/Dice_"+random_int_10+".png";
 
 
 // function change_dice();{
-    
-//   }
 
-// document.querySelector(".img1").setAttribute("src", dice_One_numbers); 
-// document.querySelector(".img2").setAttribute("src", dice_Two_numbers); 
-// document.querySelector(".img3").setAttribute("src", dice_Three_numbers); 
-// document.querySelector(".img4").setAttribute("src", dice_Four_numbers); 
-// document.querySelector(".img5").setAttribute("src", dice_Five_numbers); 
+// }
+
+document.querySelector(".img1").setAttribute("src", dice_One_numbers);
+document.querySelector(".img2").setAttribute("src", dice_Two_numbers);
+document.querySelector(".img3").setAttribute("src", dice_Three_numbers);
+document.querySelector(".img4").setAttribute("src", dice_Four_numbers);
+document.querySelector(".img5").setAttribute("src", dice_Five_numbers);
 
 
 
@@ -91,7 +141,7 @@ console.log(scc_game.filled_dice_set())
 // var random_int_7=Math.floor(Math.random() * 6)+1;
 // var random_int_8=Math.floor(Math.random() * 6)+1;
 // var random_int_9=Math.floor(Math.random() * 6)+1;
-// var random_int_10=Math.floor(Math.random() * 6)+1; 
+// var random_int_10=Math.floor(Math.random() * 6)+1;
 
 
 
@@ -99,10 +149,10 @@ console.log(scc_game.filled_dice_set())
 // var dice_Two_numbers= "dice_images/Dice_"+random_int_7+".png";
 // var dice_Three_numbers= "dice_images/Dice_"+random_int_8+".png";
 // var dice_Four_numbers= "dice_images/Dice_"+random_int_9+".png";
-// var dice_Five_numbers= "dice_images/Dice_"+random_int_10+".png"; 
+// var dice_Five_numbers= "dice_images/Dice_"+random_int_10+".png";
 
-// document.querySelector(".img1").setAttribute("src", dice_One_numbers); 
-// document.querySelector(".img2").setAttribute("src", dice_Two_numbers); 
+// document.querySelector(".img1").setAttribute("src", dice_One_numbers);
+// document.querySelector(".img2").setAttribute("src", dice_Two_numbers);
 
 
 
--- web_game/static/main.js
+++ web_game/static/main.js
@@ -1,57 +1,73 @@
 import SCC from "../../scc_game.js";
 
+// const Clicking = Object.create(null);
+
 const roll_button = document.getElementById("roll-button");
-const roll_count= document.getElementById("roll-count");
-const dice_btn1= document.getElementById("btn_1");
-const dice_btn2= document.getElementById("btn_2");
-const dice_btn3= document.getElementById("btn_3");
-const dice_btn4= document.getElementById("btn_4");
-const dice_btn5= document.getElementById("btn_5");
+const roll_count = document.getElementById("roll-count");
+const dice_btn1 = document.getElementById("btn_1");
+const dice_btn2 = document.getElementById("btn_2");
+const dice_btn3 = document.getElementById("btn_3");
+const dice_btn4 = document.getElementById("btn_4");
+const dice_btn5 = document.getElementById("btn_5");
 
 
+var roll_clicks = 0;
 
-roll_button.onclick = function() {
+roll_button.onclick = function () {
     // document.getElementById("image1").src = SCC.dice_One_numbers;
     // document.getElementById("image2").src = SCC.dice_Two_numbers;
     // document.getElementById("image3").src = SCC.dice_Three_numbers;
     // document.getElementById("image4").src = SCC.dice_Four_numbers;
     // document.getElementById("image5").src = SCC.dice_Five_numbers;
-    button_heading_count(); 
-    var roll_clicks=0;
+    button_heading_count();
     roll_clicks += 1;
 };
 
-dice_btn2.onclick = function() { 
-    console.log("CLICKED IT"); 
-}; 
+dice_btn1.onclick = function () {
+    console.log("CLICKED IT");
+};
+dice_btn2.onclick = function () {
+    console.log("CLICKED 2");
+};
+
+dice_btn3.onclick = function () {
+    console.log("CLICKED 3");
+};
+
+dice_btn4.onclick = function () {
+    console.log("CLICKED 4");
+};
+dice_btn5.onclick = function () {
+    console.log("CLICKED 5");
+};
 
 // var border_select = function(){
 //     for (var i = 0; i <= 5; i++) {
 //         dice_btn[i].addEventListener("click", function() {
 //         this.dice.add(".test_skill")
-//         }); 
+//         });
 //     };
 //     console.log("CHECKING");
 // };
 
 
-var button_heading_count =() => {
-    if ((roll_clicks % 3 ==0) || roll_clicks==(0)); {
-        roll_count.innerHTML= '3 rolls left'
-    };
-    if ((roll_clicks % 3 ==1) || roll_clicks==(1)); {
-        roll_count.innerHTML= '2 rolls left'
-    };
-    if ((roll_clicks % 3 ==2) || roll_clicks==(2)); {
-        roll_count.innerHTML= '1 roll left'
-    };
+var button_heading_count = function () {
+    if ((SCC.roll_clicks % 3 === 0) || SCC.roll_clicks === (0)) {
+        SCC.roll_count.innerHTML = "3 rolls left";
+    }
+    if ((SCC.roll_clicks % 3 === 1) || SCC.roll_clicks === (1)) {
+        roll_count.innerHTML = "2 rolls left";
+    }
+    if ((SCC.roll_clicks % 3 === 2) || SCC.roll_clicks === (2)) {
+        SCC.roll_count.innerHTML = "1 roll left";
+    }
 };
 
-//changing dice on click 
+//changing dice on click
 
 
 
-// counting click number of dice selected 
+// counting click number of dice selected
 
 // var clicks = 0;
 
@@ -66,7 +82,8 @@ var button_heading_count =() => {
 //     dice_btn3.innerHTML = three_clicks;
 //     dice_btn4.innerHTML = four_clicks;
 //     dice_btn5.innerHTML = five_clicks;
-// return stored_clicks[one_clicks,two_clicks,three_clicks,four_clicks,five_clicks]
+// return stored_clicks[one_clicks,two_clicks,three_clicks,four_clicks,
+//five_clicks]
 // };
 
 // SCC.storeClicks=(number) => {
