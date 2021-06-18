e78f00cd94517dfa530fb57cbed7e05264679887
dmj19 <73169117+dmj19@users.noreply.github.com>
2021-06-18 09:36:10 +0100

    remove console log

--- web_game/serverfunction.js
+++ web_game/serverfunction.js
@@ -1,5 +1,10 @@
 const serverfunction = Object.create(null);
 
+/* Server function is facilitating the Ajax query to the
+server through supplying the pure function to append a
+string next to the uder's input. This is called through the
+server as a promise */
+
 serverfunction.nameappend = function (name) {
     let new_name = name.toString().concat("'s score:");
     return new_name;
